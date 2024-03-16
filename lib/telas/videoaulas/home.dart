8eaeee5cae46c0f90aa596131df0dcdcd3800ecd
JonasCGN <jonsabo66@gmail.com>
2024-03-16 00:53:02 -0300

    Att

--- lib/telas/videoaulas/home.dart
+++ lib/telas/videoaulas/home.dart
@@ -38,7 +38,7 @@ class _HomeState extends State<Home> {
 			_controller = YoutubePlayerController(
 				initialVideoId: "mV_NKVG1h90",
 				flags: const YoutubePlayerFlags(
-					autoPlay: true,
+					autoPlay: false,
 					mute: false,
 				),
 			);
